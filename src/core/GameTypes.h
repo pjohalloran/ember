326eab0b3cd09d0bec3c84e5a117e5a3c381f615
pjohalloran <pjohalloran@gmail.com>
2016-12-14 22:26:56 +0000

    Changing namespaces to ember.core

--- src/core/GameTypes.h
+++ src/core/GameTypes.h
@@ -11,32 +11,35 @@
 //
 // /////////////////////////////////////////////////////////////////
 
-namespace GameHalloran
+namespace ember
 {
-	typedef char I8;
-	typedef unsigned char U8;
-	typedef short I16;
-	typedef unsigned short U16;
-	typedef int I32;
-	typedef unsigned int U32;
-	typedef long I64;
-	typedef unsigned long U64;
-	typedef float F32;
-	typedef double F64;
-	
-	// Matrix3x3 array definition (a F32 array as it is used in GLSL shaders only).
-	typedef F32 Matrix3x3[9];
-	
-	// unsigned long is 32 bits or 64 bits depending on the target architecture...
-	// However, 3rd party data chunks used in framework (zlib (.zip), .wav) use 4 bytes for DWORD no matter
-	//  what the architeture used is...  So we will explicitly define DWORD and WORD types here!
+	namespace core
+	{
+		typedef char I8;
+		typedef unsigned char U8;
+		typedef short I16;
+		typedef unsigned short U16;
+		typedef int I32;
+		typedef unsigned int U32;
+		typedef long I64;
+		typedef unsigned long U64;
+		typedef float F32;
+		typedef double F64;
+		
+		// Matrix3x3 array definition (a F32 array as it is used in GLSL shaders only).
+		typedef F32 Matrix3x3[9];
+		
+		// unsigned long is 32 bits or 64 bits depending on the target architecture...
+		// However, 3rd party data chunks used in framework (zlib (.zip), .wav) use 4 bytes for DWORD no matter
+		//  what the architeture used is...  So we will explicitly define DWORD and WORD types here!
 #if defined(__i386__)
-	typedef GameHalloran::U64 DWORD;
-	typedef GameHalloran::U16 WORD;
+		typedef ember::core::U64 DWORD;
+		typedef ember::core::U16 WORD;
 #elif defined(__x86_64__)
-	typedef GameHalloran::U32 DWORD;
-	typedef GameHalloran::U16 WORD;
+		typedef ember::core::U32 DWORD;
+		typedef ember::core::U16 WORD;
 #endif
+	}
 }
 
 #endif
