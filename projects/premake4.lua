106fc9b46d1fd2bdb7f84673016d33480f5d1494
pjohalloran <pjohalloran@gmail.com>
2016-12-14 15:25:40 +0000

    Initial commit of conan dependacy file with simple premake script.

---
+++ projects/premake4.lua
@@ -0,0 +1,23 @@
+#!lua
+
+require 'conanpremake'
+
+-- A solution contains projects, and defines the available configurations
+solution "ember-test"
+   configurations { "Debug", "Release" }
+   includedirs { conan_includedirs }
+   libdirs { conan_libdirs }
+   links { conan_libs }
+   -- A project defines one build target
+   project "ember-test"
+      kind "ConsoleApp"
+      language "C++"
+      files { "../src/**.h", "../src/**.cpp" }
+
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
