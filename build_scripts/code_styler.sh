dc75373f62c641955358e12b39935b310207b9fa
pjohalloran <pjohalloran@gmail.com>
2016-12-14 22:12:36 +0000

    Adding code styling script.

---
+++ build_scripts/code_styler.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+ASTYLE_CONFIG_FILE=""
+
+function should_style_file {
+	file_exts=( '.h' '.c' '.cpp' )
+	filename=$(basename "$1")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+	for i in "${file_exts[@]}"; do
+		if [ ".$extension" == "$i" ]; then
+			#echo "$1 is valid"
+			return 0
+		fi
+	done
+
+	return 1
+}
+
+function style_files {
+	if [ ! -d $1 ]; then
+		should_style_file $file
+		if [ $? -eq 0 ]; then
+			astyle $file --options=${ASTYLE_CONFIG_FILE}
+		fi
+	elif [ -d $1 ]; then
+		for file in $1/*; do
+			style_files $file
+		done
+	fi
+}
+
+curr_dir=`pwd`
+ASTYLE_CONFIG_FILE="$curr_dir/build_scripts/astyle.conf"
+tmp="$curr_dir/src"
+style_files $tmp
