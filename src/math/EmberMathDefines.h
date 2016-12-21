edea46e0071fd4b2d8876f8d14a581799178b0a8
pjohalloran <pjohalloran@gmail.com>
2016-12-21 17:25:53 +0000

    Fix forward declaration & linking errors.

---
+++ src/math/EmberMathDefines.h
@@ -0,0 +1,13 @@
+//
+//  EmberMathDefines.h
+//  ember
+//
+//  Created by PJ O Halloran on 21/12/2016.
+//
+//
+
+#ifndef EmberMathDefines_h
+#define EmberMathDefines_h
+
+
+#endif /* EmberMathDefines_h */
