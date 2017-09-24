a325a92ab387bddecffa57a7f6ac2f4ef40f8b15
pjohalloran <pjohalloran@gmail.com>
2017-09-24 22:53:50 +0100

    Experimental script to build all third party dependacies without conan using a shell script.

--- build_scripts/build_dependancies.sh
+++ build_scripts/build_dependancies.sh
@@ -1,42 +1,112 @@
 #!/bin/sh
 
+THIRD_PARTY_SRC_DIR="../../../src/thirdparty/"
+THIRD_PARTY_SRC_DIR_TWO="../../src/thirdparty/"
+INSTALL_DIR="../.."
+INSTALL_DIR_TWO="../"
+TMP_DIR="third_party_tmp"
+EASTL_CONFIG="MinSizeRel" # Release|Debug|RelWithDebInfo|MinSizeRel
+
 PREMAKE_DIR="projects/premake/"
-CONAN_DIR="projects/conan/"
-PREMAKE_CONAN_FILE="conanpremake.lua"
-CONAN_FILENAME="conanfile.txt"
-CONAN_INFO_FILE="conaninfo.txt"
 
-FORCE=0
+# Clean any previous
+rm -Rf "${TMP_DIR}"
+rm -Rf "${INSTALL_DIR}/lib"
+rm -Rf "${INSTALL_DIR}/include"
 
-if [ $# -gt 0 ]; then
-	if [ "$1" == "-f" ]; then
-		FORCE=1
-	fi
-fi
+# Prepare install Dirs
+mkdir "${INSTALL_DIR}/lib"
+mkdir "${INSTALL_DIR}/include"
 
-if [ $FORCE -eq 1 ]; then
-	conan install --build --file "${CONAN_DIR}${CONAN_FILENAME}"
-else
-	conan install --build=outdated --file "${CONAN_DIR}${CONAN_FILENAME}"
-fi
+# TMP CMAKE/Build Directory
+mkdir "${TMP_DIR}"
+pushd "${TMP_DIR}"
 
-if [ $? -ne 0 ]; then
-	echo "[FAILED] Installing dependancies"
-	return 1
-fi
+# GLFW
 
-mv -f "${PREMAKE_CONAN_FILE}" "${PREMAKE_DIR}"
+echo ""
+echo " #### Building GLFW ####"
+echo ""
 
-if [ $? -ne 0 ]; then
-	echo "[FAILED] copying premake dependancies"
-	return 1
-fi
+mkdir "glfw"
+pushd "glfw"
 
-mv -f "${CONAN_INFO_FILE}" "${CONAN_DIR}"
+pwd
 
-if [ $? -ne 0 ]; then
-	echo "[FAILED] Copying conaninfo"
-	return 1
-fi
+cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_DIR}" "${THIRD_PARTY_SRC_DIR}/glfw"
+cmake --build . --target install
+popd # glfw
 
-echo "[SUCCESS]"
+# EASTL
+
+echo ""
+echo " #### Building EASTL ####"
+echo ""
+
+mkdir "EASTL"
+pushd "EASTL"
+
+pwd
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_DIR}" "${THIRD_PARTY_SRC_DIR}/EASTL"
+cmake --build . --config "${EASTL_CONFIG}" --target install
+popd # EASTL
+
+# string_id
+
+echo ""
+echo " #### Building string_id ####"
+echo ""
+
+mkdir "string_id"
+pushd "string_id"
+
+pwd
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_DIR}" "${THIRD_PARTY_SRC_DIR}/string_id"
+cmake --build . --target "foonathan_string_id"
+cp -v *.a *.lib "${INSTALL_DIR}/lib/"
+mkdir "${INSTALL_DIR}/include/string_id"
+ls -l "${THIRD_PARTY_SRC_DIR}/string_id"
+cp -v "${THIRD_PARTY_SRC_DIR}/string_id/*.hpp*" "${INSTALL_DIR}/include/string_id/"
+popd # string_id
+
+# loguru
+
+echo ""
+echo " #### Building loguru ####"
+echo ""
+
+pwd
+
+ls -l "${THIRD_PARTY_SRC_DIR_TWO}/loguru"
+mkdir "${INSTALL_DIR_TWO}/include/loguru"
+cp -v "${THIRD_PARTY_SRC_DIR_TWO}/loguru/loguru.hpp" "${INSTALL_DIR_TWO}/include/loguru"
+
+# filesystem
+
+echo ""
+echo " #### Building filesystem ####"
+echo ""
+
+pwd
+
+ls -l "${THIRD_PARTY_SRC_DIR_TWO}/filesystem/filesystem"
+mkdir "${INSTALL_DIR_TWO}/include/filesystem"
+cp -v "${THIRD_PARTY_SRC_DIR_TWO}/filesystem/filesystem/*.h" "${INSTALL_DIR_TWO}/include/filesystem"
+
+# sol2
+
+echo ""
+echo " #### Building sol2 ####"
+echo ""
+
+pwd
+
+ls -l "${THIRD_PARTY_SRC_DIR_TWO}/sol2"
+mkdir "${INSTALL_DIR_TWO}/include/sol2"
+cp -v "${THIRD_PARTY_SRC_DIR_TWO}/sol2/single/sol/sol.hpp" "${INSTALL_DIR_TWO}/include/sol2"
+
+popd # TMP_DIR
+
+rm -Rf "${TMP_DIR}"
